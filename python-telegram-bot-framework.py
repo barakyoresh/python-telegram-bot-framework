f988693e9c873dd661399c454455906ee02fa882
Barak Yoresh <barakyoresh@gmail.com>
2015-08-07 16:27:30 +0300

    Started methods skeleton

--- python-telegram-bot-framework.py
+++ python-telegram-bot-framework.py
@@ -3,13 +3,27 @@ import time
 import threading
 import re
 
-class telegram_framework:
 
-    def Bot(self):
-        pass
+class Bot:
+    timer = 1
+    bad_usage_message = 'Unrecognized command or parameters.'
+    bot = None
 
-print "BARAK PLS"
+    def __init__(self, token = None, bad_usage_message = None, timer = None):
+        self.bot = telegram.Bot(token=token)
 
-bot = telegram.Bot(token='113022701:AAHsn9FiQoHHKIZ4b4OpLekFXTKZOc34lfs')
 
-print bot.getUpdates(None, 10, 0)[-1].message
+    def printLastMessage(self):
+        print self.bot.getUpdates()[-1].message
+
+
+
+
+
+def main():
+    bot = Bot(token = '113022701:AAHsn9FiQoHHKIZ4b4OpLekFXTKZOc34lfs', timer = 1)
+    bot.printLastMessage()
+
+
+if __name__ == "__main__":
+    main()
