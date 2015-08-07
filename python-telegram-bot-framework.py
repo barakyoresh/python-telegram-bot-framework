42535cda6879548d80b6a26c15b3bed193ba71ee
Barak Yoresh <barakyoresh@gmail.com>
2015-08-07 18:01:57 +0300

    added main loop

--- python-telegram-bot-framework.py
+++ python-telegram-bot-framework.py
@@ -2,10 +2,14 @@ import telegram
 import time
 import threading
 import re
+from collections import deque
 
 
 class Bot:
-    timer = 1
+    __message_queue = deque([])
+    __initiated = False
+    __offset = None
+    timer = 10
     commands = {} #name : (cb, params, description)
     bad_usage_message = 'Unrecognized command or parameters.'
     bot = None
@@ -41,11 +45,44 @@ class Bot:
         pass
 
     def activate(self):
-        pass
+        if not self.__initiated:
+            self.__initiated = True
+            updates = self.bot.getUpdates(offset = None)
+            if updates:
+                self.__offset = updates[-1].update_id + 1
+            print self.__offset
+            self.__listen_loop()
+        else:
+            raise Exception('Bot already active')
+
+
+    def __listen_loop(self):
+        print 'listen loop'
+        # add all unhandled messages to queue
+        updates = self.bot.getUpdates(offset=self.__offset)
+        if updates:
+                self.__offset = updates[-1].update_id + 1
+                for u in updates:
+                    self.__message_queue.append(u.message)
+        print self.__message_queue
+
+
+        # handle all messages in queue
+        while self.__message_queue:
+            message = self.__message_queue.popleft()
+            command_and_params = message.text.split()
+            if command_and_params[0] in self.commands:
+                self.commands[command_and_params[0]](message.things, command_and_params[1:])
+            else:
+                print 'skipped command %s, not supported' % command_and_params[0]
+                self.send_message(message.chat_id, self.bad_usage_message)
+
+        # call self
+        threading.Timer(self.timer, self.__listen_loop).start()
 
 def main():
     bot = Bot(token = '113022701:AAHsn9FiQoHHKIZ4b4OpLekFXTKZOc34lfs', timer = 1)
-    bot.printLastMessage()
+    bot.activate()
 
 
 if __name__ == "__main__":
