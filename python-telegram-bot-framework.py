d04656fbdeb6c3934076a0796368bbef02551051
daniel barros <bros.ar@gmail.com>
2015-08-07 16:43:42 +0300

    skeleton api

--- python-telegram-bot-framework.py
+++ python-telegram-bot-framework.py
@@ -12,13 +12,35 @@ class Bot:
     def __init__(self, token = None, bad_usage_message = None, timer = None):
         self.bot = telegram.Bot(token=token)
 
+    def set_timer(self, new_timer):
+        pass
 
-    def printLastMessage(self):
-        print self.bot.getUpdates()[-1].message
+    def get_timer(self):
+        pass
 
+    def get_bot_commands(self):
+        pass
 
+    def set_default_message(self, new_message):
+        pass
 
+    def get_default_message(self):
+        pass
 
+    def get_bot_name(self):
+        pass
+
+    def send_message(self, chat_id, message):
+        pass
+
+    def wait_for_message(self, chat_id):
+        pass
+
+    def add_command(self, cmd_name, cmd_description, cmd_params, cmd_cb):
+        pass
+
+    def activate(self):
+        pass
 
 def main():
     bot = Bot(token = '113022701:AAHsn9FiQoHHKIZ4b4OpLekFXTKZOc34lfs', timer = 1)
