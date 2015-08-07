92c87795912fd1be7079118d3b63f45dbcfa0738
Barak Yoresh <barakyoresh@gmail.com>
2015-08-07 15:27:06 +0300

    3rd initial commit!

--- python-telegram-bot-framework.py
+++ python-telegram-bot-framework.py
@@ -1,5 +1,16 @@
 import telegram
+import time
+import threading
+import re
+
+class telegram_framework:
+
+    def Bot(self):
+        pass
+
+
+
 
 bot = telegram.Bot(token='113022701:AAHsn9FiQoHHKIZ4b4OpLekFXTKZOc34lfs')
 
-print bot.getMe()
+print bot.getUpdates(None, 10, 0)[-1].message
